eb2c78ec9c5574b2d6ef886bb9a9458ab16a00cd
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2024-12-14 00:48:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import threading
 def run_file(filename):
     subprocess.run(["python", filename])
 
-thread1 = threading.Thread(target=run_file, args=("run_app.py",))
-thread2 = threading.Thread(target=run_file, args=("peer.py",))
+thread1 = threading.Thread(target=run_file, args=("peer.py",))
+thread2 = threading.Thread(target=run_file, args=("run_app.py",))
 
 thread1.start()
 thread2.start()
