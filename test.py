0f81e21e91a6aaa030c4a233857beaef6edb0586
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2024-12-14 00:35:33 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import subprocess
+import threading
+
+def run_file(filename):
+    subprocess.run(["python", filename])
+
+thread1 = threading.Thread(target=run_file, args=("peer.py",))
+thread2 = threading.Thread(target=run_file, args=("run_app.py",))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
